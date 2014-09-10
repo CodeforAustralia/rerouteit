5f48e488cef4248639c5f944652764602f8ff633
Christopher Chow <chris@chowie.net>
2014-09-10 21:23:20 +1000

    Remove unnecessary scaffold generated specs.

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,12 +1,10 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe HomeController, :type => :controller do
-
+describe HomeController do
   describe "GET index" do
     it "returns http success" do
       get :index
       expect(response).to be_success
     end
   end
-
 end
