1564849ac0a3c8d2e51a475bcc409d60019e1ca4
Christopher Chow <christopher.chow@kinesis.org>
2014-11-20 16:19:15 +1100

    Add a spinner.

--- app/assets/javascripts/trips.js
+++ app/assets/javascripts/trips.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+  var beforeHandler = function() {
+    showSpinner();
+    $("#result").html("");
+  };
+
   var handleResponse = function (response, textStatus, jqXHR) {
+    hideSpinner();
     $("#result").html(response);
+  };
+
+  var showSpinner = function() {
+    $("#spinner").show();
+  };
+
+  var hideSpinner = function() {
+    $("#spinner").hide();
   };
 
   $(document).keypress(function(e) {
@@ -9,6 +23,7 @@ $(document).ready(function() {
       var destination = $("#destination").val();
 
       $.ajax("/trips", {
+        beforeSend: beforeHandler,
         method: "POST",
         data: { origin: origin, destination: destination },
         success: handleResponse,
